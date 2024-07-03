9ed3aa5be70490e033d4cfa6c3d62cc3c4e16c5b
AlyonaKlekovkina <like.the.sunshine.17@gmail.com>
2024-07-03 15:33:25 +0200

    stage 3/4

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+
 def get_number_of_people():
     while True:
         number = input("Enter the number of friends joining (including you):\n")
@@ -19,7 +22,6 @@ def create_dict():
         inp = input()
         list_of_people.update({inp: 0})
         n -= 1
-    print('\n')
     total_bill = int(input('Enter the total bill value:\n'))
     split_value = round(total_bill/number_of_people, 2)
     l = len(list_of_people)
@@ -30,10 +32,21 @@ def create_dict():
     return list_of_people
 
 
+def who_is_lucky():
+    global the_list
+    get_answer = input('Do you want to use the "Who is lucky?" feature? Write Yes/No:\n')
+    if get_answer == 'Yes':
+        r = random.choice(list(the_list.keys()))
+        print(r, 'is the lucky one!')
+    else:
+        print('No one is going to be lucky')
+
+
 number_of_people = get_number_of_people()
 if number_of_people == "No one is joining for the party":
     print("No one is joining for the party")
 else:
     number_of_people = int(number_of_people)
     the_list = create_dict()
-    print(the_list)
+    who_is_lucky()
+
